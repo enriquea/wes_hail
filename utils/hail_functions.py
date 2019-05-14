150e24b67059522d1f6f83d940be61f17424cc87
enriquea <enrique.audain@gmail.com>
2019-05-14 17:26:01 +0200

    added helper function

---
+++ utils/hail_functions.py
@@ -0,0 +1,59 @@
+import hail as hl
+import hail.expr.aggregators as agg
+
+
+# run MT rows logistic regression
+def logistic_regression(mt: hl.MatrixTable,
+                        x_expr: str,
+                        response: str,
+                        covs: list,
+                        pass_through: list,
+                        extra_fields: dict) -> hl.Table:
+    # parsing covariates list
+    if len(covs) >= 1:
+        covs = [1] + [mt[field] for field in covs]
+    else:
+        covs = [1]
+
+    tb_stats = hl.logistic_regression_rows(y=mt[response],
+                                           x=mt[x_expr],
+                                           covariates=covs,
+                                           pass_through=pass_through,
+                                           test='wald')
+    # add column with additional information
+    if len(extra_fields) == 0:
+        return tb_stats
+    else:
+        return tb_stats.annotate(**extra_fields)
+
+
+# run two-tailed fisher exact test
+def compute_fisher_exact(tb: hl.Table,
+                         n_cases_col: str,
+                         n_control_col: str,
+                         total_cases: int,
+                         total_control: int,
+                         root_col_name: str,
+                         corrected_total_count: True,
+                         extra_fields: dict) -> hl.Table:
+    # compute fisher exact
+    if corrected_total_count:
+        fet = hl.fisher_exact_test(c1=hl.int32(tb[n_cases_col]),
+                                   c2=hl.int32(tb[n_control_col]),
+                                   c3=hl.int32(total_cases - tb[n_cases_col]),
+                                   c4=hl.int32(total_control - tb[n_control_col]))
+    else:
+        fet = hl.fisher_exact_test(c1=hl.int32(tb[n_cases_col]),
+                                   c2=hl.int32(tb[n_control_col]),
+                                   c3=hl.int32(total_cases),
+                                   c4=hl.int32(total_control))
+
+    tb = (tb
+          .annotate(**{root_col_name: fet})
+          .flatten()
+          )
+
+    if len(extra_fields) == 0:
+        return tb
+    else:
+        return tb.annotate(**extra_fields)

c0b8d6cb7036184ee1925d399da4d4e0481fa46e
enriquea <enrique.audain@gmail.com>
2019-05-15 14:42:32 +0200

    minor

--- utils/parser.py
+++ utils/parser.py
@@ -1,8 +1,7 @@
-# import list of vcf files (paths)
-
 import os
 
 
+# import list of vcf files (paths)
 def get_files_names(path: str, ext: str) -> list:
     list_files = []
     for (dirname, _, files) in os.walk(path):
@@ -10,5 +9,3 @@ def get_files_names(path: str, ext: str) -> list:
             if filename.endswith(ext):
                 list_files.append(os.path.abspath(os.path.join(dirname, filename)))
     return list_files
-
-

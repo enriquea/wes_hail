fc4c88ebf487ded49abd8b871bd6cdf3baa18bca
enriquea <enrique.audain@gmail.com>
2019-05-15 14:44:38 +0200

    added config and test data

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+# The absolute path of this project
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# The logs files path
+LOGS_PATH = os.path.join(ROOT_DIR, 'logs')
+
+# Hail log file
+HAIL_LOG_PATH = os.path.join(ROOT_DIR, 'logs/hail.log')

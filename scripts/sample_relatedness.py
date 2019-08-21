0012b655e55c243909381cd4c63e4a89a1c63087
enriquea <enrique.audain@gmail.com>
2019-08-21 18:26:07 +0200

    minor changes

--- scripts/sample_relatedness.py
+++ scripts/sample_relatedness.py
@@ -4,6 +4,8 @@ Compute relatedness estimates between individuals using a variant of the PC-Rela
 
 Example usage: python -m scripts.sample_relatedness --mt_path 'path/to/mt' \
                                                     --ht_output_path 'path/to/hts' \
+                                                    --common_only \
+                                                    --maf_threshold 0.05 \
                                                     --compute_pcs \
                                                     --ld_pruning \
                                                     --write_to_file
@@ -25,6 +27,17 @@ def main(args):
 
     # Read MatrixTable
     mt = hl.read_matrix_table(args.mt_path)
+
+    # force write to disk with less number of partition
+    mt = mt.repartition(100, shuffle=False)
+    mt.write('/mnt/nfs/mdatanode/hail-temp/temp.mt', overwrite=True)
+    mt = hl.read_matrix_table('/mnt/nfs/mdatanode/hail-temp/temp.mt')
+
+    if args.common_only:
+        # Filter by AF (i.e. keep common variants)
+        mt = (mt
+              .filter_rows(mt.variant_qc.AF[1] >= args.maf_threshold, keep=True)
+              )
 
     if args.sample_to_keep is not None:
         sample_table = hl.import_table(paths=args.sample_to_keep,
@@ -53,14 +66,20 @@ def main(args):
     else:
         relatedness = hl.pc_relate(mt.GT, args.individual_specific_maf, k=None, statistics='all')
 
-    # Export table to file
+    # Write/export table to file
+    tb = (relatedness
+          .flatten()
+          .key_by('i.s', 'j.s')
+          )
     date = time.strftime("%d-%m-%Y")
-    relatedness.export(output=f'{args.ht_output_path}_{date}.ht')
+    tb.write(output=f'{args.ht_output_path}_{date}.ht')
 
     # Write PCs table to file (if specified)
     if args.write_to_file:
         # Export table to file
-        relatedness.export(output=f'{args.ht_output_path}_kinship_stats_{date}.tsv')
+        tb.export(output=f'{args.ht_output_path}_kinship_stats_{date}.tsv')
+
+    hl.stop()
 
 
 if __name__ == '__main__':
@@ -73,6 +92,9 @@ if __name__ == '__main__':
     parser.add_argument('--compute_pcs', help='Use PCs as covariates (recommended)', action='store_true')
     parser.add_argument('--n_pcs', help='Number of PCs to be computed', type=int, default=10)
     parser.add_argument('--individual_specific_maf', help='MAF cutoff for filtering', type=float, default=0.01)
+    parser.add_argument('--common_only', help='Run PC-relate method on common variant set (default MAF 1%)',
+                        action='store_true')
+    parser.add_argument('--maf_threshold', help='MAF cutoff for filtering variants', type=float, default=0.01)
     parser.add_argument('--ld_pruning', help='Perform LD pruning before PCA (recommended)', action='store_true')
     parser.add_argument('--r2', help='Squared correlation threshold for LD pruning', type=float, default=0.2)
     parser.add_argument('--bp_window_size', help='Window size in bps', type=int, default=500000)
